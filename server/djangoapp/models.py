0c8a3b95322e5a6bfbd5ef07b57c989a7c2014df
rrodriguezca <robinsoncolombo@gmail.com>
2023-09-25 14:04:41 -0500

    "simple views"

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,45 +1,35 @@
 from django.db import models
 from django.utils.timezone import now
-from django.core import serializers
-import datetime
-import uuid
-import json
 
 # Create your models here.
 
 class CarMake(models.Model):
-    name = models.CharField(null=False, max_length=50)
-    description = models.TextField(null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# <HINT> Create a Car Model 
+
 class CarModel(models.Model):
-    SEDAN = "Sedan"
-    SUV = "SUV"
-    WAGON = "Wagon"
-    BIKE = "Bike"
-    OTHER = "Other"
-    CAR_CHOICES = [
-        (SEDAN, "Sedan"),
-        (SUV, "SUV"),
-        (WAGON, "Wagon"),
-        (BIKE, "Bike"),
-        (OTHER, "Other")
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(max_length=100)
+    
+    CAR_TYPES = [
+        ('Sedan', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
     ]
-
-    YEAR_CHOICES = []
-    for r in range(1969, (datetime.datetime.now().year+1)):
-        YEAR_CHOICES.append((r, r))
-
-    make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
-    name = models.CharField(null=False, max_length=50)
-    id = models.IntegerField(default=1, primary_key=True)
-    model_type = models.CharField(null=False, max_length=15, choices=CAR_CHOICES, default=SEDAN)
-    year = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+    car_type = models.CharField(max_length=10, choices=CAR_TYPES)
+    
+    year = models.DateField()
+    
+    # Add other fields as needed
 
     def __str__(self):
-        return self.name + ", " + str(self.year) + ", " + self.model_type
+        return f"{self.year} {self.make.name} {self.name}"
+
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
